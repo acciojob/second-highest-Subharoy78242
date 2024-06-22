9785ddc015be9435a49fdd2091fc10e29d7d9c95
Subharoy78242 <57645302+Subharoy78242@users.noreply.github.com>
2024-06-22 10:09:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) return -Infinity;
+
+    let first = -Infinity;
+    let second = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] < first) {
+            second = arr[i];
+        }
+    }
+
+    return (second === -Infinity) ? -Infinity : second;
+}
